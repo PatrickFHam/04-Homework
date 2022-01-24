928fb711de409b2466c5ca0aeb1693c6978f5a2f
PatrickFHam <93452044+PatrickFHam@users.noreply.github.com>
2022-01-23 20:20:46 -0500

    added two new HTMLs, independent functions work now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,104 +1,141 @@
-var countdown = document.getElementById("hud-timer");
-var progressmeter = document.getElementById("progress-meter-100");
+const countdown = document.getElementById("hud-timer");
+const progressmeter = document.getElementById("progress-meter-100");
+const question = document.getElementById("question");
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const option3 = document.getElementById("option3");
+const option4 = document.getElementById("option4");
 
-var question = document.getElementById("question");
-var choice1 = document.getElementById("choice1");
-var choice2 = document.getElementById("choice2");
-var choice3 = document.getElementById("choice3");
-var choice4 = document.getElementById("choice4");
-
-var totalQuestions
-var currentQuestion
-var completedQuestions
-var randomQuestionNumber
-
-let allQuestions = [
+const questionBank = [
 	{
 		question: 'This is question 1, and the answer should be number 4.',
-		choice1: 'Answer number 1.',
-		choice2: 'Answer number 2.',
-		choice3: 'Answer number 3.',
-		choice4: 'Answer number 4.',
+		option1: 'Answer number 1.',
+		option2: 'Answer number 2.',
+		option3: 'Answer number 3.',
+		option4: 'Answer number 4.', 
 		answer: 4,
 	},
 	{
 		question: 'This is question 2, and the answer should be number 3.',
-		choice1: 'Answer number 1.',
-		choice2: 'Answer number 2.',
-		choice3: 'Answer number 3.',
-		choice4: 'Answer number 4.',
+		option1: 'Answer number 1.',
+		option2: 'Answer number 2.',
+		option3: 'Answer number 3.',
+		option4: 'Answer number 4.',
 		answer: 3,
 	},
 	{
 		question: 'This is question 3, and the answer should be number 2.',
-		choice1: 'Answer number 1.',
-		choice2: 'Answer number 2.',
-		choice3: 'Answer number 3.',
-		choice4: 'Answer number 4.',
+		option1: 'Answer number 1.',
+		option2: 'Answer number 2.',
+		option3: 'Answer number 3.',
+		option4: 'Answer number 4.',
 		answer: 2,
 	},
 	{
 		question: 'This is question 4, and the answer should be number 1.',
-		choice1: 'Answer number 1.',
-		choice2: 'Answer number 2.',
-		choice3: 'Answer number 3.',
-		choice4: 'Answer number 4.',
+		option1: 'Answer number 1.',
+		option2: 'Answer number 2.',
+		option3: 'Answer number 3.',
+		option4: 'Answer number 4.',
 		answer: 1,
 	},
 	{
 		question: 'This is question 5, and the answer should be number 2.',
-		choice1: 'Answer number 1.',
-		choice2: 'Answer number 2.',
-		choice3: 'Answer number 3.',
-		choice4: 'Answer number 4.',
+		option1: 'Answer number 1.',
+		option2: 'Answer number 2.',
+		option3: 'Answer number 3.',
+		option4: 'Answer number 4.',
 		answer: 2,
 	},
 	{
 		question: 'This is question 6, and the answer should be number 3.',
-		choice1: 'Answer number 1.',
-		choice2: 'Answer number 2.',
-		choice3: 'Answer number 3.',
-		choice4: 'Answer number 4.',
+		option1: 'Answer number 1.',
+		option2: 'Answer number 2.',
+		option3: 'Answer number 3.',
+		option4: 'Answer number 4.',
 		answer: 3,
 	},
+	{
+		question: 'This is question 7, and the answer should be number 4.',
+		option1: 'Answer number 1.',
+		option2: 'Answer number 2.',
+		option3: 'Answer number 3.',
+		option4: 'Answer number 4.',
+		answer: 4,
+	}
 ]
 
-totalQuestions = allQuestions.length;
+var totalQuestions = questionBank.length;
+var currentQuestion = {}
+var currentQuestionNumber = 0
+var completedQuestions = 0
+var randomQuestionOrder = [];
 
-function startCountdown (z) {
+function startCountdown (t) {
 	  // Sets interval in variable
-		var secondsLeft = z;
+		var secondsLeft = t;
 		countdown.children[1].textContent = secondsLeft;
 		var timerInterval = setInterval(function() {
 			secondsLeft--;
 			countdown.children[1].textContent = secondsLeft;
 	
 			if(secondsLeft === 0) {
-				// Stops execution of action at set interval
 				clearInterval(timerInterval);
-				// Calls function to create and append image
+				// Show "timeout" message.
 				console.log("timer is done!");
+				timeOut ();
 			}
 	
 		}, 1000);
 }
 
-function randomQuestionNumber () {
-	var random
-	random = Math.floor(Math.random()*allQuestions.length);
-	console.log(random);
-	return random;
+function timeOut (){
+		window.location.assign("./outoftime.html");
 }
 
+function randomizeQuestionOrder () {
+	randomQuestionOrder = questionBank.sort(() => (Math.random() > .5) ? 1 : -1);
+	console.log(randomQuestionOrder);
+	return randomQuestionOrder
+}		
+
 function showQuestion(x) {
-	question.children[0].textContent = allQuestions[x-1].question;
-	choice1.textContent = allQuestions[x-1].choice1;
-	choice2.textContent = allQuestions[x-1].choice2;
-	choice3.textContent = allQuestions[x-1].choice3;
-	choice4.textContent = allQuestions[x-1].choice4;
+	question.children[0].textContent = questionBank[x-1].question;
+	option1.textContent = questionBank[x-1].option1;
+	option2.textContent = questionBank[x-1].option2;
+	option3.textContent = questionBank[x-1].option3;
+	option4.textContent = questionBank[x-1].option4;
+}
+
+function clearQuestion () {
+	question.children[0].textContent = '';
+	option1.textContent = '';
+	option2.textContent = '';
+	option3.textContent = '';
+	option4.textContent = '';
 }
 
 function changeProgMeterPercent(a,b) {
 	percentFinished = Math.round((a / b) * 100) + '%';
 	progressmeter.style.width = percentFinished;
 }
+
+function startQuiz (){
+	startCountdown(30);
+	randomizeQuestionOrder();
+	showQuestion(randomQuestionOrder[0]);
+}
+
+// maybe not use the t,x, ... just make the start button work
+
+// START BUTTON
+// start countdown
+// randomize the question set
+// show the active question 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
